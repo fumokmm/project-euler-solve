111a8915f3eee8757c087d1fab23fa5bb398cd1c
fumokmm <fumo.kmm@gmail.com>
2012-06-21 07:00:13 +0900

    add comment

--- problems/20/problem20.groovy
+++ problems/20/problem20.groovy
@@ -1,3 +1,18 @@
+/*
+ * http://projecteuler.net/problem=20
+ *
+ * Problem 20
+ * 21 June 2002
+ * --------------------------------------------------------
+ * n! means n × (n − 1) × ... × 3 × 2 × 1
+ *
+ * For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,
+ * and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+ *
+ * Find the sum of the digits in the number 100!
+ * --------------------------------------------------------
+ */
+
 def sumOfTheFactDigits(int num) {
   if (num < 1) return 0
   def fact = { n ->
@@ -6,8 +21,8 @@ def sumOfTheFactDigits(int num) {
   fact(num).toString().toList()*.toInteger().sum()
 }
 
-println(
-  (0..100).collect {
-    new Tuple(it, sumOfTheFactDigits(it))
-  }.last()[1]
-)
+assert (0..100).collect {
+  new Tuple(it, sumOfTheFactDigits(it))
+}.last()[1] == 648
+
+
